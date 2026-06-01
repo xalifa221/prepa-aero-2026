6a832f28a8ccb54eaaa514c4515f30619e54d57b
kkhalifa <xalifa.221@gmail.com>
2026-06-01 13:43:51 +0200

    Premier commit : setup environnement Python

---
+++ test.py
@@ -0,0 +1,9 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import cantera as ct
+import sklearn
+
+print("NumPy:", np.__version__)
+print("Cantera:", ct.__version__)
+print("scikit-learn:", sklearn.__version__)
+print("✅ Tout fonctionne !")
