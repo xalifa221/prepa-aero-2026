3ba66054e6a251cd574da84ae90823f10bd5b1c2
kkhalifa <xalifa.221@gmail.com>
2026-06-01 21:12:09 +0200

    fichier modifie par Khalifa

--- test.py
+++ test.py
@@ -2,8 +2,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 import cantera as ct
 import sklearn
-
+import math 
 print("NumPy:", np.__version__)
 print("Cantera:", ct.__version__)
 print("scikit-learn:", sklearn.__version__)
+print("scikit-learn:", sklearn.__version__)
 print("✅ Tout fonctionne !")
+#Je veux modifier
+
+a=2
